159ced0c0d9e23b84dce9681a7dc1ca8b0dd445f
Daria <dariaurbanik@gmail.com>
2019-04-24 15:17:42 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let num = 1
+
+while(num <= 100) {
+    if (num % 3 ===0 && num %5 === 0){
+        console.log("FizzBuzz") 
+        num++
+      } else if (num % 5 === 0) {
+         console.log("Buzz")
+         num++
+      } else if (num% 3 === 0){
+         console.log("Fizz")
+         num++
+      } else {
+          console.log(num)
+          num++
+      }
+}
